954974807f2d98a5bd47e3de40b01b565777c7e0
RajatKumar-gsrk <rajatkumarrk47.rk@gmail.com>
2025-01-11 10:11:06 +0530

    Basics of document Scanner have been worked through now just need to convert image to pdf

---
+++ README.md
@@ -0,0 +1,4 @@
+#PYTHON Document Scanner Project
+<br><br>
+passdown path to the image and with help of machine vision crops the document<br><br>
+Ideal results make sure most area of image is covered by document, and noise is limited
---
+++ scanner.py
@@ -0,0 +1,79 @@
+import imutils.perspective
+import numpy as np
+import cv2
+import matplotlib.pyplot as mp
+import imutils
+
+def main():
+    #opening image
+    original_image = cv2.imread("./images/03.jpg")
+
+    #displaying image file
+    #cv2.imshow("original_image", original_image)
+
+    #resized image
+    resized_image = resize_image(original_image, 1000)
+    #cv2.imshow("resized_image", resized_image)
+
+    #turned graycale
+    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+   # cv2.imshow("gray_image", gray_image)
+
+    #blurred
+    blured_image = cv2.GaussianBlur(gray_image,(5, 5), 0)
+    #cv2.imshow("blured_image", blured_image)
+
+    #detecting edges
+    edge_detected_image = cv2.Canny(blured_image, 50, 200)
+   # cv2.imshow("EDGE_image", edge_detected_image)
+
+    #broadning edges
+    seed = np.ones((4, 4), np.uint8)
+    dialated_image = cv2.dilate(edge_detected_image, seed)
+   # cv2.imshow("dialated_image", dialated_image)
+
+    #closing small gaps
+    no_gap_image = cv2.morphologyEx(dialated_image, cv2.MORPH_CLOSE, seed)
+   # cv2.imshow("no_gap_image", no_gap_image)
+    
+    #outling edges on image
+    four_points = getCornerPoints(no_gap_image)
+    print([four_points])
+    cv2.drawContours(resized_image, [four_points], -1, (255, 0, 0), 3)
+   # cv2.imshow("outline_image", resized_image)
+
+    #cropped image
+    cropped_image = imutils.perspective.four_point_transform(resized_image, four_points)
+    cv2.imshow("Cropped_image", cropped_image)
+
+
+
+    #closing all windows
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#image resizing function
+def resize_image(image, new_width):
+    old_height, old_width, channel = image.shape
+    aspect_ratio = old_height / old_width
+    new_height = int(aspect_ratio * new_width)
+    image_new_size = (new_height, new_width)
+
+    image = cv2.resize(image, image_new_size)
+    return image
+
+
+def getCornerPoints(image):
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours = sorted(contours, key = cv2.contourArea, reverse = True) #puts largest area first
+
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+
+        if len(approx) == 4: #found largest closed are with 4 corners
+            return np.squeeze(approx)
+
+if __name__ == "__main__":
+    main()
